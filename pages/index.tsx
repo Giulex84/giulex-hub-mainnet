3fff86efcf84ebaed6bc5a396353f8758b882353
Giulex84 <giulianom.gm@gmail.com>
2026-02-17 23:07:47 +0100

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Giulex Hub</title>
+        <script src="https://sdk.minepi.com/pi-sdk.js"></script>
+      </head>
+      <body style={{ fontFamily: "Arial", textAlign: "center", padding: 30 }}>
+        <h1>Giulex Hub</h1>
+        <p>Home (Next.js) attiva ✅</p>
+
+        <p>
+          <a href="/privacy.html">Privacy</a> | <a href="/terms.html">Terms</a>
+        </p>
+      </body>
+    </html>
+  );
+}
