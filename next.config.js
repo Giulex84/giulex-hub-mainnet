45035b31989e4f0a760c832b9024168733f3d65c
Giulex84 <giulianom.gm@gmail.com>
2026-09-21 01:16:46 +0200

    Prepare Arena 1.2.0 for Mainnet review

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     return [{
       source: '/(:path*)',
       headers: [
-        { key: 'Content-Security-Policy', value: "frame-ancestors https://*.minepi.com https://minepi.com https://*.pinet.com https://*.socialchain.app;" },
+        { key: 'Content-Security-Policy', value: "object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors https://*.minepi.com https://minepi.com https://*.pinet.com https://*.socialchain.app;" },
         { key: 'X-Content-Type-Options', value: 'nosniff' },
         { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
         { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
